7fc4ed527d576d5ccbed18ab507b04c7355c37cf
senoralikz <alexjm825@gmail.com>
2022-08-03 19:55:57 -0400

    created checkout page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,20 @@
 import Image from 'next/image'
 import { MenuIcon, SearchIcon, ShoppingCartIcon } from '@heroicons/react/outline'
 import { signIn, signOut, useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
 
 const Header = () => {
   // get user info that is signed in to session
   const { data: session } = useSession()
+
+  const router = useRouter()
 
   return (
     <header>
       {/* Top nav */}
       <div className='flex items-center bg-amazon_blue p-1 flex-grow py-2'>
         <div className='mt-2 flex items-center flex-grow sm:flex-grow-0'>
-          <Image src='https://links.papareact.com/f90' width={150} height={40} objectFit='contain' className='cursor-pointer' />
+          <Image onClick={() => router.push('/')} src='https://links.papareact.com/f90' width={150} height={40} objectFit='contain' className='cursor-pointer' />
         </div>
         {/* Search bar */}
         <div className='hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500'>
@@ -29,7 +32,7 @@ const Header = () => {
             <p>Returns</p>
             <p className='font-extrabold md:text-sm'>& Orders</p>
           </div>
-          <div className='relative link flex items-center'>
+          <div onClick={() => router.push('/checkout')} className='relative link flex items-center'>
             <span className='absolute top-0 right-0 md:right-6 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold'>0</span>
 
             <ShoppingCartIcon className='h-10' />
