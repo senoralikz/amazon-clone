f61a56d3a229ff7ede8d48650ce1b2187aa7a165
senoralikz <alexjm825@gmail.com>
2023-03-22 19:26:10 -0400

    replaced amazon logo image link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,70 +1,106 @@
-import Image from 'next/image'
-import { MenuIcon, SearchIcon, ShoppingCartIcon } from '@heroicons/react/outline'
-import { signIn, signOut, useSession } from 'next-auth/react'
-import { useRouter } from 'next/router'
-import { useSelector } from 'react-redux'
-import { selectItems } from '../slices/basketSlice'
+import Image from "next/image";
+import {
+    MenuIcon,
+    SearchIcon,
+    ShoppingCartIcon,
+} from "@heroicons/react/outline";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useSelector } from "react-redux";
+import { selectItems } from "../slices/basketSlice";
 
 const Header = () => {
-  // get user info that is signed in to session
-  const { data: session } = useSession()
+    // get user info that is signed in to session
+    const { data: session } = useSession();
 
-  // grab items in shopping cart store
-  const items = useSelector(selectItems)
+    // grab items in shopping cart store
+    const items = useSelector(selectItems);
 
-  const router = useRouter()
+    const router = useRouter();
 
-  return (
-    <header className='absolute sticky top-0 z-50 shadow-xl'>
-      {/* Top nav */}
-      <div className='flex items-center bg-amazon_blue p-1 flex-grow py-2'>
-        <div className='mt-2 flex items-center flex-grow sm:flex-grow-0'>
-          <Image onClick={() => router.push('/')} src='https://links.papareact.com/f90' width={150} height={40} objectFit='contain' className='cursor-pointer' />
-        </div>
-        {/* Search bar */}
-        <div className='hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500'>
-          <input type="text" className='p-2 h-full w-6 flex-grow rounded-l-md flex-shrink focus:outline-none' />
-          <SearchIcon className='h-12 p-4' />
-        </div>
+    return (
+        <header className="absolute sticky top-0 z-50 shadow-xl">
+            {/* Top nav */}
+            <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
+                <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
+                    <Image
+                        onClick={() => router.push("/")}
+                        src="https://imgur.com/lz0n3pA"
+                        width={150}
+                        height={40}
+                        objectFit="contain"
+                        className="cursor-pointer"
+                    />
+                </div>
+                {/* Search bar */}
+                <div className="hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500">
+                    <input
+                        type="text"
+                        className="p-2 h-full w-6 flex-grow rounded-l-md flex-shrink focus:outline-none"
+                    />
+                    <SearchIcon className="h-12 p-4" />
+                </div>
 
-        {/* Right side of search bar */}
-        <div className='text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap'>
-          <div onClick={() => !session ? router.push('/signin') : signOut()} className='link'>
-            <p>{session ? `Hello ${session.user.name}` : 'Sign In'}</p>
-            <p className='font-extrabold md:text-sm'>Account & Lists</p>
-          </div>
-          <div onClick={() => router.push('/orders')} className='link'>
-            <p>Returns</p>
-            <p className='font-extrabold md:text-sm'>& Orders</p>
-          </div>
-          <div onClick={() => router.push('/checkout')} className='relative link flex items-center'>
-            {items.length > 0 && (
-            <span className='absolute top-0 right-0 md:right-6 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold'>{items.length}</span>
-            )}
+                {/* Right side of search bar */}
+                <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
+                    <div
+                        onClick={() =>
+                            !session ? router.push("/signin") : signOut()
+                        }
+                        className="link"
+                    >
+                        <p>
+                            {session ? `Hello ${session.user.name}` : "Sign In"}
+                        </p>
+                        <p className="font-extrabold md:text-sm">
+                            Account & Lists
+                        </p>
+                    </div>
+                    <div
+                        onClick={() => router.push("/orders")}
+                        className="link"
+                    >
+                        <p>Returns</p>
+                        <p className="font-extrabold md:text-sm">& Orders</p>
+                    </div>
+                    <div
+                        onClick={() => router.push("/checkout")}
+                        className="relative link flex items-center"
+                    >
+                        {items.length > 0 && (
+                            <span className="absolute top-0 right-0 md:right-6 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">
+                                {items.length}
+                            </span>
+                        )}
 
-            <ShoppingCartIcon className='h-10' />
-            <p className='hidden md:inline font-extrabold md:text-sm mt-2'>Cart</p>
-          </div>
-        </div>
-      </div>
+                        <ShoppingCartIcon className="h-10" />
+                        <p className="hidden md:inline font-extrabold md:text-sm mt-2">
+                            Cart
+                        </p>
+                    </div>
+                </div>
+            </div>
 
-      {/* Bottom nav */}
-      <div className='flex items-center bg-amazon_blue-light text-white text-sm space-x-3 p-2 pl-6'>
-        <p className='link flex items-center'>
-          <MenuIcon className='h-6 mr-1' />
-          All</p>
-          <p className='link'>Prime Video</p>
-          <p className='link'>Amazon Business</p>
-          <p className='link'> Today's Deals</p>
-          <p className='link hidden lg:inline-flex'>Electronics</p>
-          <p className='link hidden lg:inline-flex'>Food & Grocery</p>
-          <p className='link hidden lg:inline-flex'>Prime</p>
-          <p className='link hidden lg:inline-flex'>Buy Again</p>
-          <p className='link hidden lg:inline-flex'>Shopper Toolkit</p>
-          <p className='link hidden lg:inline-flex'>Health & Personal Care</p>
-      </div>
-    </header>
-  )
-}
+            {/* Bottom nav */}
+            <div className="flex items-center bg-amazon_blue-light text-white text-sm space-x-3 p-2 pl-6">
+                <p className="link flex items-center">
+                    <MenuIcon className="h-6 mr-1" />
+                    All
+                </p>
+                <p className="link">Prime Video</p>
+                <p className="link">Amazon Business</p>
+                <p className="link"> Today's Deals</p>
+                <p className="link hidden lg:inline-flex">Electronics</p>
+                <p className="link hidden lg:inline-flex">Food & Grocery</p>
+                <p className="link hidden lg:inline-flex">Prime</p>
+                <p className="link hidden lg:inline-flex">Buy Again</p>
+                <p className="link hidden lg:inline-flex">Shopper Toolkit</p>
+                <p className="link hidden lg:inline-flex">
+                    Health & Personal Care
+                </p>
+            </div>
+        </header>
+    );
+};
 
-export default Header
+export default Header;
